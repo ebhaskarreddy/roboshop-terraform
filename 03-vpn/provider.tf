dfa8e7c6fdd48c3e021bb1ee63fda47862968b7f
ebhaskarreddy <ebhaskarreddy967@gmail.com>
2024-01-17 14:20:56 +0530

    terraform-test-vpc

---
+++ 03-vpn/provider.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "5.31.0" # AWS provider version, not terraform version
+    }
+  }
+
+  backend "s3" {
+    bucket         = "bhaskar75-remote-state"
+    key            = "default"
+    region         = "us-east-1"
+    dynamodb_table = "bhaskar75-locking"
+  }
+}
+
+provider "aws" {
+  region = "us-east-1"
+}
--- 04-ec2/ec2.tf
+++ 04-ec2/ec2.tf
@@ -1,16 +1,17 @@
-module "mongodb"{
-  source  = "terraform-aws-modules/ec2-instance/aws"
-  name = "${local.ec2_name}-mongodb"
+module "mongodb" {
+  source                 = "terraform-aws-modules/ec2-instance/aws"
+  ami = data.aws_ami.centos8.id
+  name                   = "${local.ec2_name}-mongodb"
   instance_type          = "t3.small"
-  vpc_security_group_ids = [data.aws_ssm_parameter.mongodb_sg_id]
-  subnet_id              = local.databse_subnet_id
+  vpc_security_group_ids = [data.aws_ssm_parameter.mongodb_sg_id.value]
+  subnet_id              = local.database_subnet_id
   tags = merge(
     var.common_tags,
     {
-      component = "mongodb"
+      Component = "mongodb"
     },
     {
-      name = "${local.ec2_name}-mongodb"
+      Name = "${local.ec2_name}-mongodb"
     }
   )
 }
